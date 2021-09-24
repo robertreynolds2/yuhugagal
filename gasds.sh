d6e0f006ca0045ec4594a054510a15ba0bec0755
robertreynolds2 <64372884+robertreynolds2@users.noreply.github.com>
2021-09-24 18:07:32 +0700

    Update gasds.sh

--- gasds.sh
+++ gasds.sh
@@ -1,14 +1,28 @@
-rm -rf ngrok ngrok.zip ng.sh > /dev/null 2>&1
-echo "======================="
-echo "Download ngrok"
-echo "======================="
-wget -O ngrok.zip https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
-unzip ngrok.zip > /dev/null 2>&1
-read -p "Paste Ngrok Authtoken: " CRP
-./ngrok authtoken $CRP 
-./ngrok tcp --region ap 4000 &>/dev/null &
-apt update && apt upgrade -y
-apt install ubuntu-desktop -y
-wget -O nomachine.deb https://bit.ly/nomachinev7
-sudo apt install ./nomachine.deb
+docker pull danchitnis/xrdp:fedora-xfce
+clear
+echo -e "$123\n$123\n" | sudo passwd
+rm -rf ngrok  ngrok.zip  ng.sh > /dev/null 2>&1
+wget -O ng.sh https://bit.ly/GCngrok > /dev/null 2>&1
+chmod +x ng.sh
+./ng.sh
+clear
+echo "===================================="
+echo "Install RDP"
+echo "===================================="
+echo "===================================="
+echo "Start RDP"
+echo "===================================="
+sudo sed -i.bak '/fi/a lxde-session \n' /etc/xrdp/startwm.sh
+sudo service xrdp start > /dev/null 2>&1
+echo "===================================="
+echo "Username : root"
+echo "Password : 123"
+echo "RDP Address:"
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
+echo "===================================="
+echo "===================================="
+echo "Don't close this tab RDP runs 50 hours"
+echo "Keep support akuh.net thank you"
+echo "===================================="
+echo "===================================="
+docker run -it -p 3388:3389 danchitnis/xrdp:fedora-xfce root 123 yes > /dev/null 2>&1
